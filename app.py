1dbdee9e741a665902e132d411fd013ec3faf878
Alyssa Smith <alyssa.dev.smith@gmail.com>
2021-07-04 17:20:14 +0000

    added srt function, changed page format

--- app.py
+++ app.py
@@ -4,14 +4,49 @@ from flask import *
 from glob import glob
 from string import ascii_lowercase, ascii_uppercase, digits
 import os.path
-from json import load
+from json import load, loads
+from io import BytesIO
+from math import floor
 
 app = Flask(__name__)
+
+def s2ts(inp):
+    # seconds to timestamps: converts e.g 0 to 00:00:00,000, 2.427 to 00:00:02,427
+    if inp < 0:
+        inp = 0
+    s,ms = divmod(inp,1)
+    m,s = divmod(s,60)
+    h,m = divmod(m,60)
+    return f"{h:02.0f}:{m:02.0f}:{s:02.0f},{floor(ms*100):02.0f}"
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, "static"),
                   '63546997.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/<channel>/<video_id>.srt")
+def srt(channel, video_id):
+    if channel[:2] != "UC" or any(c not in ascii_lowercase + ascii_uppercase + digits + "_" for c in channel) or len(channel) != 24:
+        return "", 404
+    if any(c not in ascii_lowercase + ascii_uppercase + digits + "_-" for c in video_id) or len(video_id) != 11:
+        return "", 404
+    try:
+        with open(os.path.join("cache", channel, video_id + ".json")) as f:
+            data = load(f)
+    except:
+        return "", 404
+    if len(data["subtitles"]["Items"]) == 0:
+        flash("Video has no subtitles yet")
+        return redirect(url_for("index"))
+    # I hate this very much
+    sub = loads(loads(data["subtitles"]["Items"][0]["subtitles"]["S"]))
+    fmt = "{n}\n{ts1} --> {ts2}\n{line}\n"
+    out = []
+    for n,l in enumerate(sub):
+        ts1 = s2ts(l["start"])
+        ts2 = s2ts(l["end"])
+        out.append(fmt.format(n=n+1, ts1=ts1, ts2=ts2, line=l["text"]))
+    return send_file(BytesIO("\n".join(out).encode("utf-8")), mimetype="text/plain", attachment_filename=video_id + ".srt", as_attachment=True)
 
 @app.route("/<channel>/")
 def view(channel):
